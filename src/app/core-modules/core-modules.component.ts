7c16a83c75fcdf0d4b09c9703f4745cbd0d12d8a
Oluwatayoh <ojotayoh2@gmail.com>
2019-05-17 17:38:32 +0100

    update

--- src/app/core-modules/core-modules.component.ts
+++ src/app/core-modules/core-modules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-core-modules',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreModulesComponent implements OnInit {
 
+  open = true;
+  close = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("mySidenav").style.boxShadow = "10px 4px 15px rgba(73, 82, 104, 0.1)";
+    document.getElementById("mySidenav").style.borderRight = "none";
+    this.open = false;
+    this.close = true;
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "70px";
+    document.getElementById("mySidenav").style.boxShadow = "none";
+    document.getElementById("mySidenav").style.borderRight = "1px solid rgba(158, 167, 187, 0.2)";
+    this.open = true;
+    this.close = false;
+  }
 }
--- src/app/core-modules/core-modules.module.ts
+++ src/app/core-modules/core-modules.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CoreModulesComponent } from './core-modules.component';
 import { CoreModuleRoute } from './core-modules.route';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { MaterialModule } from '../shared-module/material-module';
 
 
 @NgModule({
-    declarations: [CoreModulesComponent, DashboardComponent, TopBarComponent],
+    declarations: [CoreModulesComponent, DashboardComponent],
     imports: [CommonModule, CoreModuleRoute, MaterialModule]
 })
 export class CoreModule { }
