ed59ea58f7c9805b998e8fb886f3d2532425c24d
Oluwatayoh <ojotayoh2@gmail.com>
2019-05-23 00:16:34 +0100

    make it better

--- server.js
+++ server.js
@@ -11,4 +11,6 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log('Server Started');
+})
