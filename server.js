ec92e71c3023d3c24066ac68452328c6c8ede893
Oluwatayoh <ojotayoh2@gmail.com>
2019-05-24 18:32:42 +0100

    update

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/iecc'));
+app.use(express.static(__dirname + '/dist/IECCProject'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/iecc/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/IECCProject/index.html'));
 });
 
 // default Heroku PORT
