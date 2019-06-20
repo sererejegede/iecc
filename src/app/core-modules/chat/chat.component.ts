d7d0e69171ff7dbe71844e8a39a1d838cf4ddc68
Arokoyu Olalekan <arokoyuolalekan@gmail.com>
2019-06-20 10:42:21 +0100

    fetching of messages on the chat component.

--- src/app/core-modules/chat/chat.component.ts
+++ src/app/core-modules/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { pageloaderService } from 'src/app/services/pageloaderService';
 import { UserService } from 'src/app/services/user.service';
 import { CoolLocalStorage } from 'angular2-cool-storage';
+import {ChatService} from '../../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -14,14 +15,19 @@ export class ChatComponent implements OnInit {
   showChatBody = false;
   staffs: any;
   user;
+  public messages: any[] = [];
 
   selectedStaff: any;
   constructor( private _pageloaderService: pageloaderService,
     private _userService: UserService,
-    private _locker: CoolLocalStorage,) { }
+    private _chatService: ChatService,
+    private _locker: CoolLocalStorage) { }
 
   ngOnInit() {
-    this._pageloaderService.setTitle('Chat')
+    this._pageloaderService.setTitle('Chat');
+    this.getStaffs();
+  }
+  public getStaffs() {
     this._userService.getStaffs().subscribe(
       (payloadU: any) => {
         this.staffs = payloadU.data;
@@ -32,11 +38,24 @@ export class ChatComponent implements OnInit {
       }
     );
   }
-
-  onSelectedUser(staff){
+  public getUserChats(id) {
+    this.user = this._locker.getObject('selectedUser');
+    console.log('This.user ', this.user);
+    this._chatService.getMessages(id).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.messages = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  onSelectedUser(staff) {
     this.selectedStaff = staff;
     this.user = this._locker.getObject('selectedUser');
     this.showChatBody = true;
+    this.getUserChats(staff._id);
   }
 
 }
